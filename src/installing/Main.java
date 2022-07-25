ed69f797790738fac6ecd54cab36e1105c833026
Denis Fedorov <ssboshen@yandex.ru>
2022-07-25 10:46:07 +0300

    Removing to new folder, add some changes

---
+++ src/installing/Main.java
@@ -0,0 +1,11 @@
+package installing;
+
+import java.io.File;
+
+public class Main {
+    StringBuilder operationResult = new StringBuilder();
+    File newDir = new File("Games//src");
+    if(newDir.mkdir()){
+
+    }
+}
